87413313c858413ae5654d36327ca7dae46a59f9
KayteeKlink <kayteeklink@gmail.com>
2020-05-04 16:50:38 +0000

    feat: Unit Test, wrote onSuccess test for ListViewModelTests.kt

--- app/src/test/java/com/example/animals/ListtViewModelTest.kt
+++ app/src/test/java/com/example/animals/ListtViewModelTest.kt
@@ -4,15 +4,19 @@ import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.animals.di.AppModule
 import com.example.animals.di.DaggerViewModelComponent
+import com.example.animals.model.Animal
 import com.example.animals.model.AnimalApiService
 import com.example.animals.util.SharePreferencesHelper
 import com.example.animals.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
@@ -33,6 +37,8 @@ class ListtViewModelTest {
         Mockito.mock(Application::class.java) //app is not created as lateinit var bc we need it right away to instantiate the ListViewModel class below
 
     var listViewModel = ListViewModel(application)
+
+    private val key = "Test key"
 
     @Before
     fun setup() {
@@ -65,5 +71,23 @@ class ListtViewModelTest {
         //create newThread() and mainThread() in ListViewModel and have it return immediately here for our tests
         RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
+    }
+
+    @Test
+    fun getAnimalSuccess() {
+        Mockito.`when`(prefs.getApiKey()).thenReturn(key)
+        //if we get an api call then we return a key (when we already have a key in our system?)
+        val animal = Animal("cow", null, null, null, null, null, null)
+        val animalList = listOf(animal) //gives us the list of animals we want to return when the viewmodel does a call to getAnimals
+
+        val testSingle = Single.just(animalList)
+
+        Mockito.`when`(animalService.getAnimals(key)).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.animals.value?.size)
+        Assert.assertEquals(false, listViewModel.loadError.value) //checks that we're not getting any errors
+        Assert.assertEquals(false, listViewModel.loading.value) //assert that loading boolean has false value also
     }
 }
